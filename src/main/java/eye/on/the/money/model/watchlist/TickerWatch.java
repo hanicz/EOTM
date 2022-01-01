529e0cac5c71e90444ac7a25b1835637a2f129a5
Hanicz Tamás <hanicz20@gmail.com>
2022-01-01 13:18:26 +0100

    identity

--- src/main/java/eye/on/the/money/model/watchlist/TickerWatch.java
+++ src/main/java/eye/on/the/money/model/watchlist/TickerWatch.java
@@ -18,7 +18,7 @@ import javax.persistence.*;
 @NoArgsConstructor
 public class TickerWatch {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne

6c4965561a4464e31eab1207728aba5a127a7566
Tamas Hanicz <hanicz20@gmail.com>
2023-11-10 16:24:21 +0100

    circle ci tests

--- backend/src/test/java/eye/on/the/money/EotmApplicationTest.java
+++ backend/src/test/java/eye/on/the/money/EotmApplicationTest.java
@@ -5,12 +5,14 @@ import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = EotmApplication.class)
+@ActiveProfiles("test")
 class EotmApplicationTest {
 
     @Autowired
